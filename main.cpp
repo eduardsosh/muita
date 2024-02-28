75407d95c2d51ebd589a353f9f07b56bb26fb4a6
Eduards <eduardsosh@gmail.com>
2024-02-28 15:55:40 +0200

    File read and print

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include<iostream>
+#include<fstream>
 
 using namespace std;
 
-int main(){
-        
+template <typename T>
+void print_arr(const T &array , int arr_lenght){
+    for(int i=0; i<arr_lenght;i++){
+        cout << array[i];
+    }
+}
 
-        return 0;
+
+int main(){
+    ifstream inFile("customs.in");
+
+    if (!inFile.is_open()){
+        cerr << "Error opening file!";
+        return 1;
+    }
+
+
+    // Read first 4 numbers into a int[]
+    int first_data[4];
+
+    for(int i=0; i<4;i++){
+        inFile >> first_data[i];
+    }
+
+    int p_muitnieku_n = first_data[0];
+    int n_mutnieku_n = first_data[1];
+    int default_p_time = first_data[2];
+    int default_n_time = first_data[3];
+
+    print_arr<int>(first_data,4);
+
+
+    return 0;
 }
