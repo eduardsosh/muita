3588d579cf6b942599bbcc1d6ecd1d19f1ac6a53
eduardsosh01~ <eduardsosh@gmail.com>
2024-03-16 17:59:54 +0200

    Basic setup done

--- main.cpp
+++ main.cpp
@@ -29,11 +29,64 @@ int main(){
     }
 
     int p_muitnieku_n = first_data[0];
-    int n_mutnieku_n = first_data[1];
+    int n_muitnieku_n = first_data[1];
     int default_p_time = first_data[2];
     int default_n_time = first_data[3];
 
     print_arr<int>(first_data,4);
+
+    //Izveidosim muitniekus un ievadisim vinu laikus
+
+    //char vaimuitnieks;
+    char muitnieka_tips;
+    int muitnieka_i;
+    int muitnieka_laiks;
+    
+    int *p_muitnieku_laiki = new int[p_muitnieku_n];
+    int *n_muitnieku_laiki = new int[n_muitnieku_n];
+
+    for(int i = 0; i<p_muitnieku_n; i++){
+        p_muitnieku_laiki[i] = default_p_time;
+    }
+
+    for(int i = 0; i<n_muitnieku_n; i++){
+        n_muitnieku_laiki[i] = default_n_time;
+    }
+    // Muitnieki un vinu esosain klients
+    // Pie katra muitnieka bus cilveka_id + muitnieka apkalposanas laiks
+    // Ja nakama cilveka id parsniedz kadu no esosajiem laikiem, tad tas cilveks tiek izvadits faila
+    // un tiek ievietots nakamais brivaja vieta.
+
+    // Izveidojam 2 masivus kuri bus apkalposanas laiki
+    int *p_punkti = new int[p_muitnieku_n];
+    int *n_punkti = new int[n_muitnieku_n];
+
+    char tips;
+    int cilveka_id = 0;
+    inFile >> tips;
+
+    if(tips == 'T'){
+        while(tips == 'T'){
+            inFile >> muitnieka_tips >> muitnieka_i >> muitnieka_laiks;
+            if(muitnieka_tips == 'P'){
+                p_muitnieku_laiki[muitnieka_i - 1] = muitnieka_laiks;
+            }
+            if(muitnieka_tips == 'N'){
+                n_muitnieku_laiki[muitnieka_i - 1] = muitnieka_laiks;
+            }
+            inFile >> tips;
+        }
+    }
+    
+    while(tips != 'X'){
+        inFile >> cilveka_id;
+        // tgd mums ir tips un cilveka id
+        // sakas pats algoritms.
+
+        
+
+        inFile >> tips;
+    }
 
 
     return 0;
